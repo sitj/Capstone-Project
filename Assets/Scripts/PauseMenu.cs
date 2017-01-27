255c8be6cbf6f309d48ba7066667122b0778ff1b
sitj <sitj9228@yahoo.com>
2017-01-27 17:35:13 -0600

    Add files via upload

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour {
+	
+	public bool isPaused;
+
+	public GameObject pauseMenuCanvas;
+
+	// Update is called once per frame
+	void Update () {
+		
+		if (isPaused) {
+			pauseMenuCanvas.SetActive (true);
+			Time.timeScale = 0f;
+		} else {
+			pauseMenuCanvas.SetActive (false);
+			Time.timeScale = 1f;
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			isPaused = !isPaused;
+		}
+	
+	}
+	public void Resume()
+	{
+		isPaused = false;
+	}
+	public void Quit()
+	{
+		Application.Quit();
+	}
+}
