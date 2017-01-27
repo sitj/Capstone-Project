0476d19f777e9f6372a357506a96852eaba48982
sitj <sitj9228@yahoo.com>
2017-01-27 17:36:02 -0600

    Add files via upload

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour {
     public float speed;
@@ -7,35 +8,94 @@ public class PlayerControl : MonoBehaviour {
     private float moveVelocity;
     private bool grounded = true;
     private SpriteRenderer mySpriteRenderer;
+    public Animator anim;
+    public Collider2D attacktriggerleft;
+    public Collider2D attacktriggerright;
+    public Collider2D portaltrigger;
+    public object portal;
+    public string nextscene;
+    private bool attacking = false;
+    private bool change = false;
+
 
     void Start() {
         mySpriteRenderer = GetComponent<SpriteRenderer>();
-
+        anim = GetComponent<Animator>();
     }
+    void Awake()
+    {
+        attacktriggerleft.enabled = false;
+        attacktriggerright.enabled = false;
+    }
+
     void Update () {
-        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W)) {
-            print(grounded);
+		if(Time.timeScale == 0f)
+		{
+			return;
+		}
+        print(change);
+        if (Input.GetKeyDown(KeyCode.F) && attacking == false)
+        {
+            attacking = true;
+            if (mySpriteRenderer.flipX == false)
+            {
+                attacktriggerright.enabled = true;
+            }
+            else if (mySpriteRenderer.flipX == true)
+            {
+                attacktriggerleft.enabled = true;
+            }
+            anim.SetBool("attack", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.F) && attacking == true)
+        {
+            attacking = false;
+            attacktriggerleft.enabled = false;
+            attacktriggerright.enabled = false;
+            anim.SetBool("attack", false);
+        }
+
+        float move = Input.GetAxis("Horizontal");
+        if (move == 0) {
+            anim.SetBool("IsMoving", false);
+        }
+
+        moveVelocity = 0;   
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+
             if (grounded) {
                 GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jump);
             }
         }
-        moveVelocity = 0;
 
-        if (Input.GetKey(KeyCode.A)) {
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            anim.SetBool("IsMoving", true);
             moveVelocity = -speed;
-            mySpriteRenderer.flipX = true;
-            
         }
-        if (Input.GetKey(KeyCode.D)) {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            anim.SetBool("IsMoving", true);
             moveVelocity = speed;
+        }
+
+        if (moveVelocity > 0 && mySpriteRenderer.flipX == true)
+        {
             mySpriteRenderer.flipX = false;
+        }
+        else if(moveVelocity < 0 && mySpriteRenderer.flipX == false){
+            mySpriteRenderer.flipX = true;
         }
         GetComponent<Rigidbody2D>().velocity = new Vector2(moveVelocity, GetComponent<Rigidbody2D>().velocity.y);
 	}
+
     void OnTriggerEnter2D() {
         grounded = true;
+        anim.SetBool("IsGrounded", true);
     }
+
     void OnTriggerExit2D() {
         grounded = false;
+        anim.SetBool("IsGrounded", false);
     }
+    
 }
